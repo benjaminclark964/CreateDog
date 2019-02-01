672e907281d0541c2ffe75052995deec5794f72e
benjaminclark964 <46797974+benjaminclark964@users.noreply.github.com>
2019-01-31 21:19:04 -0700

    Create DogBark.java

---
+++ DogBark.java
@@ -0,0 +1,36 @@
+
+/**
+ * 
+ * @author Benjamin Clark
+ * How dogs barks
+ */
+public class DogBark {
+
+	/**
+	 * Loud dog bark
+	 */
+	public String loudBark() {
+		return("BARK!!!");
+	}
+	
+	/**
+	 * Quite dog bark
+	 */
+	public String quiteBark() {
+		return("bark");
+	}
+	
+	/**
+	 * Average dog bark
+	 */
+	public String averageBark() {
+		return("Bark");
+	}
+	
+	/**
+	 * Multiple barks
+	 */
+	public String multipleBarks() {
+		return("Bark, Bark, Bark!!!");
+	}
+}
