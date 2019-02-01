dd0c74e37d3c0e6501c071363398ac71f2ac36e8
benjaminclark964 <46797974+benjaminclark964@users.noreply.github.com>
2019-01-31 21:18:33 -0700

    Create DogBreed.java

---
+++ DogBreed.java
@@ -0,0 +1,73 @@
+
+/**
+ * 
+ * @author Benjamin Clark
+ * Different type of dog breeds
+ */
+public class DogBreed {
+
+	/**
+	 * Pit bull breed
+	 */
+	public String pitBull() {
+		return("Pitbull");
+	}
+	
+	/**
+	 * Australian Shephard breed
+	 * @return 
+	 */
+	public String australianShephard() {
+		
+		return "Australian Shephard";
+	}
+	
+	/**
+	 * Siberian Husky breed
+	 */
+	public String siberianHusky() {
+		return("Siberian Husky");
+	}
+	
+	/**
+	 * Maltese breed
+	 */
+	public String maltese() {
+		return("Maltese");
+	}
+	
+	/**
+	 * Poodle breed
+	 */
+	public String poodle() {
+		return("Poodle");
+	}
+	
+	/**
+	 * Tibetan Mastiff breed
+	 */
+	public String tibetanMastiff() {
+		return("Tibetan Mastiff");
+	}
+	
+	/**
+	 * Weiner dog breed
+	 */
+	public String weinerDog() {
+		return("Weiner");
+	}
+	
+	/**
+	 * Golden Retriever breed
+	 */
+	public String goldenRetriever() {
+		return("Golden Retriever");
+	}
+	
+	/**
+	 * Lab breed
+	 */
+	public String lab() {
+		return("Lab");
+	}
+}
