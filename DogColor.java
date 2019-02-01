288d9fa7d89ba76d05c1b029d615de3558c54034
benjaminclark964 <46797974+benjaminclark964@users.noreply.github.com>
2019-01-31 21:17:56 -0700

    Create DogColor.java

---
+++ DogColor.java
@@ -0,0 +1,70 @@
+/**
+ * 
+ * @author Benjamin Clark
+ * The color of the dog
+ */
+public class DogColor {
+	
+	/**
+	 * The color black
+	 */
+	public String black() {
+		return("Black");
+	}
+	
+	/**
+	 * the color gray
+	 */
+	public String gray() {
+		return("Gray");
+	}
+	
+	/**
+	 * the color blue
+	 */
+	public String blue() {
+		return("Blue");
+	}
+	
+	/**
+	 * the color Yellow
+	 */
+	public String yellow() {
+		return("Yellow");
+	}
+	
+	/**
+	 * the color brown
+	 */
+	public String brown() {
+		return("Brown");
+	}
+	
+	/**
+	 * the color white
+	 */
+	public String white() {
+		return("White");
+	}
+	
+	/**
+	 * the color purple
+	 */
+	public String purple() {
+		return("Purple");
+	}
+	
+	/**
+	 * the color red
+	 */
+	public String red() {
+		return("Red");
+	}
+	
+	/**
+	 * the color green
+	 */
+	public String green() {
+		return("Green");
+	}
+}
